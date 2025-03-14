e419c3d2b09eeac5f9ce60e8aad52d2b6e6ed1ff
ndqhuy <huy.ndq15160@sinhvien.hoasen.edu.vn>
2025-03-14 19:28:29 +0700

    session-1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello world';
+})->name('greeting'); // name of route
+
+Route::get('/greeting/{name}', function ($name) {
+    return 'Hello ' . $name;
+});
+
+Route::get('/hiU', function () {
+    return redirect('/greeting');
+});
+
+Route::get('/hi2', function () {
+    return redirect()->route('greeting'); // redirect to route has name 'greeting'
+});
+
+Route::get('/Home', function () {
+    return redirect('/');
+});
+
