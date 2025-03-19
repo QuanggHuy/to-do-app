3ff5cab50526a74052e923a189f196e37d2a8fed
ndqhuy <huy.ndq15160@sinhvien.hoasen.edu.vn>
2025-03-19 19:29:55 +0700

    session-3-model

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-class Task{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description, // optional
-        public bool $completed)
-        {
-        }
-}
+use App\Models\Task;
+// class Task{
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description, // optional
+//         public bool $completed)
+//         {
+//         }
+// }
         
 
-$tasks = [
-    new Task(1, 'Uống đủ nước', 'Uống 4 lít 1 ngày', null, false), 
-    new Task(2, 'Dậy lúc 6h30', 'Dậy lúc 6h30', null, true),
-    new Task(3, 'Làm bài tập về nhà', 'Làm bài tập PHP', null, false),
-    new Task(4, 'Học từ vựng tiếng anh', 'Học 100 từ mỗi tuần', null, true),
-];
+// $tasks = [
+//     new Task(1, 'Uống đủ nước', 'Uống 4 lít 1 ngày', null, false), 
+//     new Task(2, 'Dậy lúc 6h30', 'Dậy lúc 6h30', null, true),
+//     new Task(3, 'Làm bài tập về nhà', 'Làm bài tập PHP', null, false),
+//     new Task(4, 'Học từ vựng tiếng anh', 'Học 100 từ mỗi tuần', null, true),
+// ];
 
-Route::get('/', function () use ($tasks) {
-    return view('index', ['tasks' => $tasks]);
-});
+// Route::get('/', function () use ($tasks) {
+//     return view('index', ['tasks' => $tasks]);
+// });
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id); // Lấy task đầu có id trùng với id truyền vào
-    if(!$task){
-        return redirect('404');
-    }
-    return view('detail', ['task' => $task]);
-})->name('tasks.detail');
+// Route::get('/tasks/{id}', function ($id) use ($tasks) {
+//     $task = collect($tasks)->firstWhere('id', $id); // Lấy task đầu có id trùng với id truyền vào
+//     if(!$task){
+//         return redirect('404');
+//     }
+//     return view('detail', ['task' => $task]);
+// })->name('tasks.detail');
 
-Route::fallback(function () {
-    return "Khong tim thay trang";
-});
+// Route::fallback(function () {
+//     return "Khong tim thay trang";
+// });
 
 // Route::get('about', function(){
 //     return view('index',['name' => 'About']);
@@ -60,3 +61,12 @@ Route::fallback(function () {
 //     return redirect('/');
 // });
 
+Route::get('/', function () {
+    $task = Task::latest('title')->get();
+    return view('index',['tasks'=>$task]);
+});
+
+Route::get('/tasks/{id}', function ($id) {
+    $task = Task::findOrFail($id);
+    return view('details',['task'=>$task]);
+})->name('tasks.detail');    
